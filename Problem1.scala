9c6c4e69cd98b753a247416357b514027b7e2661
ajg029 <a.gorman029@gmail.com>
2023-02-10 17:08:56 -0600

    Part D done

--- Problem1.scala
+++ Problem1.scala
@@ -31,8 +31,6 @@ object Problem1 {
     // def outshuffle[A](l1: List[A], f:A => List[List[A]], h:A => List[A]): List[A] = (l1, f, h) match {
     /*  */
     def outshuffle[A](l1: List[A]): List[A] = l1 match {
-        // case (_, _, _) => Nil
-
         // case //
         case _ =>
             val lists = split(l1, l1.length/2)
@@ -44,19 +42,34 @@ object Problem1 {
     def inshuffle[A](l1: List[A]): List[A] = l1 match {
         case _ =>
 
-
-
-            Nil
+            val lists = split(l1, l1.length/2)
+            shuffle(lists(1), lists(0))
     }
 
     /* Problem 1 Part D */
+    def nshuffle[A] (l1: List[A], n: Int)(f:List[A] => List[A]): List[A] = (l1, n, f) match {
+        case (_, _, _) =>
+            // f(l1)
+            if (n == 0) l1
+            // else nshuffle(l1, n-1)(f)
+            else {
+                // val l2 = nshuffle(l1, n-1)(f)
+                // val l2 = outshuffle(l1)
+                nshuffle(outshuffle(l1), n-1)(f)
+            }
+    }
+
+    /* Problem 1 Part E */
+    def howManyShuffles[A] (l1: List[A], l2: List[A])(f:List[A] => List[A]): Int = (l1, l2, f) match {
+        case (_, _, _) => 2
+    }
 
 
         
 
 
     def main(args: Array[String]): Unit = {
-        println("Begin Testing Assignment 2")
+        // println("Begin Testing Assignment 2")
 
         /* Test Problem 1 Part A */
         val l1 = List(0, 2, 4, 6)
@@ -74,10 +87,27 @@ object Problem1 {
 
 
         /* Test Problem 1 Part C */
-        val prob1_partc_Outshuffle = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-        println(outshuffle(prob1_partc_Outshuffle))
+        // val prob1_partc_Inshuffle = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        // println(prob1_partc_Inshuffle.take(2).tail)
+        // println(prob1_partc_Inshuffle.drop(8).take(1))
 
-        val prob1_partc_Inshuffle = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        // val remaining = prob1_partc_Inshuffle.take(1)
+        // println(remaining)
+
+        // val test = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        // val l5 = inshuffle(test)
+        // println(l5)
+
+        // val remaining2 = prob1_partc_Inshuffle.
+
+        /* Test Problem 1 Part D */
+        val l6 = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        println(nshuffle(l6, 10)(outshuffle))
+
+
+
+
 
 
 
