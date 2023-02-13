d68ba45244ec833c1c9572ffc66ead5c86a280de
ajg029 <a.gorman029@gmail.com>
2023-02-12 18:25:17 -0600

    Asn 2 done need to clean

--- Problem3.scala
+++ Problem3.scala
@@ -50,19 +50,39 @@ object Problem3 {
 
 
     def main(args: Array[String]): Unit = {
-        // println(luhnDouble(6))
-        // println(luhnDouble(3))
+        println("Testing Problem 3")
+
+        /* Testing Part A */
+        println("")
+        println("Testing Part A")
+        println("Luhn double of 6 should be 3, answer returned from luhnDouble(): "+luhnDouble(6))
+        if (luhnDouble(6)!=3) println("error in luhnDouble()")
+
+        println("Luhn double of 3 should be 6, answer returned from luhnDouble(): "+luhnDouble(3))
+        if (luhnDouble(6)!=3) println("error in luhnDouble()")
+
+        /* Testing Part B */
+        println("")
+        println("Testing Part B")
+        val list1 = List(1, 2, 3, 4, 5)
+        val functionList1 = List(luhnDouble _, luhnDouble _, luhnDouble _)
+        println("altmap() function given a List of (1, 2, 3, 4, 5) and a function list" +
+          "of (luhnDouble_, luhnDouble_, luhnDouble_) should return a list of (2, 4, 6, 8, 1), " +
+          "answer returned from altMap()"+altMap(list1)(functionList1))
+        
+        /* Testing Part C */
+        
 
         // val l1 = List(1, 2, 3, 4)
         // val f1 = List(luhnDouble _, luhnDouble _, luhnDouble _, luhnDouble _)
         // println(altMap(l1)(f1))
 
-        val l1 = List(1, 2, 3, 4)
-        val f1 = List(luhnDouble _, luhnDouble _, luhnDouble _)
-        println(altMap(l1)(f1))
+        // val l1 = List(1, 2, 3, 4)
+        // val f1 = List(luhnDouble _, luhnDouble _, luhnDouble _)
+        // println(altMap(l1)(f1))
 
-        val l2 =  altMap(l1)(f1)
+        // val l2 =  altMap(l1)(f1)
 
-        println(luhn(l1))
+        // println(luhn(l1))
     }
 }
